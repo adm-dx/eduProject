46741d9d6a4b1cb19b0ad0c401e7c0266f43463f
Roman Miller <44736500+adm-dx@users.noreply.github.com>
2021-11-18 14:36:50 +0300

    Update LeftSideMenuTest.java

--- src/test/java/ru/qa/education/LeftSideMenuTest.java
+++ src/test/java/ru/qa/education/LeftSideMenuTest.java
@@ -25,7 +25,7 @@ public class LeftSideMenuTest {
     }
 
     @Test
-    public void adminLoginTest() {
+    public void leftSideMenuTest() {
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
